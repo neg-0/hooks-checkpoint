90c355e5834099e860a48c26e08833a2837e358a
Dustin Stringer <dustin.w.stringer@gmail.com>
2021-10-13 15:33:50 -0400

    Added a bit of styling to product list

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,9 +1,18 @@
 import ProductCard from "./ProductCard";
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
 
 export default function ProductList({ products }) {
     return (
-        <div>
-            {products.map(product => <ProductCard key={product.id} product={product} />)}
-        </div>
+        <Box sx={{ flexGrow: 1 }}  >
+            <Grid container
+                spacing={2}
+                direction="row"
+                justifyContent="center"
+                alignItems="top">
+                {products.map(product => <Grid item xs={2} width={"200px"}><ProductCard key={product.id} product={product} /></Grid>)}
+            </Grid>
+        </Box>
     )
 }
